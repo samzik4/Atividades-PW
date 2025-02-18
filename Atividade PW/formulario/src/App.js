6f5c12148c6d48a9b265c6cd6954222dc2346a6f
Samuel Felipe <samuel.felipe04004@gmail.com>
2025-02-17 21:27:43 -0400

    Add files via upload

---
+++ Atividade PW/formulario/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Formulario from "./components/Formulario.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Formulario />
+    </div>
+  );
+}
+
+export default App;
